99482bc180aeb3f06a1319b2b5c7caf1034bd2fb
Varsha Sham Aaynure <66114947+VarshaAaynure@users.noreply.github.com>
2022-06-30 00:04:10 +0530

    Update and rename log.txt to addTwoNumbers.py

---
+++ addTwoNumbers.py
@@ -0,0 +1,11 @@
+# Program to add two numbers 
+num1 = 10
+num2 = 20
+
+# Add numbers
+total = num1 + num2
+
+# Print the result
+print("Sum of {0} and {1} is {2}".format(num1,num2,total))
+# or 
+print("Sum of",num1,"and",num2,"is",total)
