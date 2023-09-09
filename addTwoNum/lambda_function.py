aa6c123cb7ccd41750b50ad083607674f95c4498
Vrsha-Aaynure <varshaainure98@gmail.com>
2023-09-09 14:49:56 +0000

    added new code

---
+++ addTwoNum/lambda_function.py
@@ -0,0 +1,8 @@
+add_numbers = lambda x, y: x+y
+
+num1 = 10
+num2 = 20
+
+result = add_numbers(num1, num2)
+
+print("Sum of {0} and {1} is {2}".format(num1, num2, result))
---
+++ addTwoNum/operator_add.py
@@ -0,0 +1,10 @@
+# add two numbers by using operator.add() method
+
+num1 = 10
+num2 = 20
+
+import operator 
+
+sum = operator.add(num1, num2)
+
+print ("Sum of {0} and {1} is {2}".format(num1, num2, sum))
